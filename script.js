6e1ba6217eee6eafc1bb642f498649c10ddc9974
Misha <miszabrandt@gmail.com>
2021-09-28 14:49:43 +0200

    ver1

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("penis");
+console.log(2+2);
+console.log(5>2 && 2==2 && !(3>4));
+
+let penis = 5;
+
+if(5!=penis){
+    console.log("penis");
+} else{
+    console.log("penis 2");
+}
+
+if(penis==2){
+    console.log("cosinnego")
+}else if(penis==3){
+    console.log("dupa")
+}else if(penis=="cipa"){
+    console.log("cipa")
+}else{
+    console.log("nico")
+}
+
+let cipa = 4;
+
+console.log(10 / 3)
+
+if(cipa==8){
+    console.log("hurra")   
+}
+else{
+    console.log("nico")
+}
+console.log(2+2)
+
+
+alert("Hej penisku!!!");
